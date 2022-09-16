89497fc97e6c0df732d64e9ca5b0bc2865fab976
Osama ayub <ayubosama416@gmail.com>
2022-09-16 12:55:50 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import DataTable from './components/DataGrid';
+import './App.css';
+function App() {
+  return (
+    <div className='App'>
+      <DataTable />
+    </div>
+  );
+}
+
+export default App;
